df23c98820c47875ff3cf7a37a418a1613a570de
conor <conor@localhost.localdomain>
2025-03-24 22:40:42 -0600

    Modificacion

--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -1,20 +1,24 @@
 package Arrays;
 
+import java.util.Arrays;
 import java.util.Iterator;
 
 public class Main {
 	public static void main(String args[]) {
 		int[] numeros = new int[5];
 
-		numeros[0] = 1;
+		numeros[0] = 12;
 		numeros[1] = Integer.valueOf(2);
-		numeros[2] = 3;
-		numeros[3] = 4;
-		numeros[4] = 5;
+		numeros[2] = 35;
+		numeros[3] = 42;
+		numeros[4] = 15;
 
 		for (int numero : numeros) {
 			System.out.println("recorrido numeros: " + numero);
 		}
+		
+		Arrays.sort(numeros);
+		
 		System.out.println("\n");
 		for (int i = 0; i < numeros.length; i++) {
 			System.out.println("recorrido numeros: " + numeros[i] + " posicion: " + i);
